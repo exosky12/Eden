5a4affdd1493ca98ee471c860eeb481c3d3dd0ed
exosky12 <102408092+exosky12@users.noreply.github.com>
2025-02-03 07:04:08 -0800

    Update sign-in.js

--- js/sign-in.js
+++ js/sign-in.js
@@ -3,12 +3,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebas
 import {  getAuth, browserSessionPersistence, signInWithEmailAndPassword, setPersistence, signInWithRedirect, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCIeIK2P73AJwoR2Qb2Nac_xJAAn_qUeGA",
-  authDomain: "eden-cdee4.firebaseapp.com",
-  projectId: "eden-cdee4",
-  storageBucket: "eden-cdee4.appspot.com",
-  messagingSenderId: "166124310796",
-  appId: "1:166124310796:web:74391c4bca9e92c3e53929"
+  // add your firebase config
 };
 
 // Initialize Firebase
